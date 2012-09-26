e1a00bb8f5ffd864e1ba993f5863c7592d7c9ed9
Katie Hawthorne <kathryne.h@gmail.com>
2012-09-25 21:15:35 -0700

    Merging user

--- user.js
+++ user.js
@@ -3,18 +3,28 @@
 * Models users that are working on the current project.
 * Includes role in the project and light biographical information.
 *
-* The methods defined pertain to the manipulation of activity    *objects.
+* The methods defined pertain to the manipulation of activity objects
 *
 */
 
-var User = function(userid, first, last, role, imgLocation) {
+var User = function(first, last, username, role, imgLocation, permissions) {
 	this.firstName = first;
 	this.lastName = last;
-	this.role = role;
-	this.picture = imgLocation;
-	this.userID = userid;
 
-	/*Each user will have a string array of the titles of their activities*/
+
+	//Check to see if database has username first, if not then set the given username
+	this.username = username;
+
+	this.role = role;
+
+	//We will load image files into Image objects here
+	userImage = new Image();
+	this.userImage.src = ""imgLocation"";
+
+	//A string array of permissions will be set
+	this.permissions = permissions[];
+
+	//Each user will have a string array of the titles of their activities
 	this.activityLog = [];
 }
 
@@ -33,11 +43,16 @@ User.prototype.updateImg = function(newImgLocation){
 
 User.prototype.addActivity() = function(item){
 	this.activityLog.push(item);
-	item.setOwner = this;
 }
 
 User.prototype.removeActivity() = function(item){
 	this.activityLog[this.getIndexOf(item)] = null;
+}
+
+User.prototype.updatePermissions() = function(permissions[i]){
+	this.permissions[i] = permissions[i];
+}
+
 
 }
 
