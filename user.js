3b80e3bd3fddbd9209816af1d754a251ce8cd65e
Kronkle <mkronk7@gmail.com>
2012-09-25 22:24:59 -0300

    IMG objs, permissions, usernames

--- user.js
+++ user.js
@@ -3,17 +3,28 @@
 * Models users that are working on the current project.
 * Includes role in the project and light biographical information.
 *
-* The methods defined pertain to the manipulation of activity    *objects.
+* The methods defined pertain to the manipulation of activity objects
 *
 */
 
-var User = function(first, last, role, imgLocation) {
+var User = function(first, last, username, role, imgLocation, permissions) {
 	this.firstName = first;
 	this.lastName = last;
+	
+	
+	//Check to see if database has username first, if not then set the given username
+	this.username = username;
+	
 	this.role = role;
-	this.picture = imgLocation;
+	
+	//We will load image files into Image objects here
+	userImage = new Image();
+	this.userImage.src = ""imgLocation"";
+	
+	//A string array of permissions will be set
+	this.permissions = permissions[];
 
-	/*Each user will have a string array of the titles of their activities*/
+	//Each user will have a string array of the titles of their activities
 	this.activityLog = [];
 }
 
@@ -36,5 +47,12 @@ User.prototype.addActivity() = function(item){
 
 User.prototype.removeActivity() = function(item){
 	this.activityLog[this.getIndexOf(item)] = null;
+}
+
+User.prototype.updatePermissions() = function(permissions[i]){
+	this.permissions[i] = permissions[i];
+}
+
+
 }
 
